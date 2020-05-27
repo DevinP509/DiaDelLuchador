0b21a56f13033069ac5f3170a51bbe22ec2902e0
JacksonMossman <54289017+JacksonMossman@users.noreply.github.com>
2020-05-27 17:30:12 -0500

    Made Attack changed Jump with extra Gravity Added Wasp enemy Behavior

---
+++ Assets/Scripts/PlayerMovementBehavior.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Diagnostics;
+public class PlayerMovementBehavior : MonoBehaviour
+{
+    //Refrences to the rigidbody
+    private Rigidbody rigi;
+
+    //Used to dictate the players movements
+    private float moveInput;
+
+    //Base player movement
+    public float speed;
+
+    //Determines how high the player can jump
+    public float jumpForce;
+    //right is false
+    //left is truth
+    private bool facing= false;
+    //Determines what ground is
+    private bool isGrounded;
+
+    public GameObject punchBox;
+
+    Stopwatch stopwatch = new Stopwatch();
+    
+    void Start()
+    {
+        //Get the Rigibody of the player
+        rigi = GetComponent<Rigidbody>();
+        stopwatch.Start();
+        
+    }
+
+    void FixedUpdate()
+    {
+        //moveInput is equel to the Horizontal control
+        moveInput = Input.GetAxisRaw("Horizontal");
+        if(moveInput >0)
+        {
+            facing = false;
+            punchBox.transform.localPosition = new Vector3(1,0,0);
+        }
+        else if (moveInput < 0)
+        {
+            facing = true;
+            punchBox.transform.localPosition = new Vector3(-1, 0, 0);
+        }
+
+        //How fast the player moves
+        rigi.velocity = new Vector3(moveInput * speed, rigi.velocity.y -.3f, 0);
+    }
+
+    //Old Jumping System if we decide to use Diagonal platforms
+
+    //void OnCollisionEnter(Collision collision)
+    //{
+        
+    //    switch (collision.gameObject.tag)
+    //    {
+    //        //If the gameObject is ground set isGrounded to true
+    //        case "Ground":
+    //            isGrounded = true;
+    //             break;
+    //        default:
+    //            isGrounded = false;
+    //            break;
+    //    }
+    //    if (!collision.gameObject)
+    //    {
+    //        isGrounded = false;
+    //    }
+    //}
+
+    void Update()
+    {
+        if(punchBox.activeSelf == true && stopwatch.ElapsedMilliseconds > 100)
+        {
+            punchBox.SetActive(false);
+        }
+        //If the player is on ground and space key is pressed
+        if (rigi.velocity.y == 0 && Input.GetKeyDown(KeyCode.Space))
+        {
+            //Player jumps
+            rigi.velocity = Vector3.up * jumpForce;
+            isGrounded = false;
+        }
+        if(Input.GetKeyDown(KeyCode.R) && stopwatch.ElapsedMilliseconds > 200)
+        {
+            punchBox.SetActive(true);
+            stopwatch.Restart();
+        }
+
+    }
+}
---
+++ Assets/Scripts/WaspEnemyBehavior.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaspEnemyBehavior : MonoBehaviour
+{
+    public GameObject player;
+    public int speed;
+    public int Damage;
+    public int Health;
+    public Camera GameCamera;
+    public Rigidbody rb;
+    private Renderer rendererer;
+    private BoxCollider collider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameCamera = UnityEngine.Camera.main;
+        rendererer = GetComponent<Renderer>();
+        collider = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        ChasePlayer();
+
+
+        if (Health <= 0)
+        {
+            die();
+        }
+    }
+    private void ChasePlayer()
+    {
+        if (CheckIfOnScreen() == true)
+        {
+
+            //Get this agent's position
+            Vector3 pos = transform.position;
+            //Get the position of the target agent
+            Vector3 targetPos = player.transform.position;
+
+            //Calculate the vector describing the direction to the target and normalize it
+            Vector3 direction = targetPos - pos;
+            direction = direction.normalized;
+            //Multiply the direction by the speed we want the agent to move
+            Vector3 force = direction * speed;
+            //Subtract the agent's current velocity from the result to get the force we need to apply
+            force = force - rb.velocity;
+
+            //Return the force
+            rb.AddForce(force);
+        }
+
+    }
+    public void TakeDamage(int damgage)
+    {
+
+    }
+    private bool CheckIfOnScreen()
+    {
+
+        Plane[] planes = GeometryUtility.CalculateFrustumPlanes(GameCamera);
+        if (GeometryUtility.TestPlanesAABB(planes, collider.bounds))
+            return true;
+        else
+            return false;
+    }
+
+
+
+    private void die()
+    {
+
+    }
+}
