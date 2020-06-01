84c944888c75e862059020b860a5d05d16c6e6b8
HunterTrahan <Trahan.hunter@yahoo.com>
2020-06-01 14:19:22 -0500

    Added value keeping script

---
+++ Assets/Scripts/ValueKeepingBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Holds the code for keeping track of the players score and lives
+/// </summary>
+public class ValueKeepingBehavior : MonoBehaviour
+{
+    /// <summary>
+    /// Set the base value for the score and lives
+    /// </summary>
+    public float score = 0;
+    public float lives = 3;
+
+    /// <summary>
+    /// Create a refrence to the text values
+    /// </summary>
+    public Text scoreValue;
+    public Text livesValue;
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    void Update()
+    {
+        scoreValue.text = "Score: " + score.ToString();
+        livesValue.text = "Lives: " + lives.ToString();
+    }
+}
