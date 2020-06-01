8033e33c553de4f28040451670789a212e9c8fe8
JacksonMossman <54289017+JacksonMossman@users.noreply.github.com>
2020-06-01 16:16:35 -0500

    add quetzal and fireballs

---
+++ Assets/FireballLauncher.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class FireballLauncher : MonoBehaviour
+{
+    public GameObject player;
+
+    public GameObject fireballPrefab;
+
+    public int speed;
+
+    public int firerate;
+
+    private Rigidbody rb;
+
+    Stopwatch stopwatch = new Stopwatch();
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        stopwatch.Start();
+    }
+    void followplayer()
+    {
+
+        //Get this agent's position
+        Vector3 pos = transform.position;
+        //Get the position of the target agent
+        Vector3 targetPos = player.transform.position;
+
+        //Calculate the vector describing the direction to the target and normalize it
+        Vector3 direction = targetPos - pos;
+        direction = direction.normalized;
+        //Multiply the direction by the speed we want the agent to move
+        Vector3 force = direction * speed;
+        //Subtract the agent's current velocity from the result to get the force we need to apply
+        force = force - rb.velocity;
+
+        //Return the force
+        rb.AddForce(force);
+    }
+    private void fire()
+    {
+        if (stopwatch.ElapsedMilliseconds > (10000 / firerate))
+        {
+            Instantiate(fireballPrefab,transform);
+
+            
+            stopwatch.Restart();
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        followplayer();
+        fire();
+    }
+}
