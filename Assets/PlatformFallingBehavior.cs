b8efead50143118fd0004c444f49c4deae27b2bc
JacksonMossman <54289017+JacksonMossman@users.noreply.github.com>
2020-06-12 06:19:31 -0500

    Added Platform Spawner

---
+++ Assets/PlatformFallingBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class PlatformFallingBehavior : MonoBehaviour
+{
+    public float fallSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PlatformDestroyer"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+
+    private void FixedUpdate()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y-fallSpeed, transform.position.z);
+    }
+}
---
+++ Assets/PlatformSpawnerBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+
+public class PlatformSpawnerBehavior : MonoBehaviour
+{
+    public List<GameObject> PlatformPreFabs;
+    public float SpawnTime;
+    public float platformFallRate;
+    Stopwatch stopwatch = new Stopwatch();
+     UnityEngine.Random random = new Random();
+    // Start is called before the first frame update
+    void Start()
+    {
+        stopwatch.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(stopwatch.ElapsedMilliseconds /1000 >= SpawnTime)
+        {
+            GameObject platform = PlatformPreFabs[UnityEngine.Random.Range(0, PlatformPreFabs.Count - 1)];
+            platform.GetComponent<PlatformFallingBehavior>().fallSpeed = platformFallRate;
+            Instantiate(platform, transform);
+            
+            stopwatch.Restart();
+        }
+    }
+}
