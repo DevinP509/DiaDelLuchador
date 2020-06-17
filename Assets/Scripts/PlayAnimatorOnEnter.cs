fbdd26940d21a6489b936d9a396649a07021e8b4
DevinP509 <54288889+DevinP509@users.noreply.github.com>
2020-06-17 17:44:44 -0500

    starting platform and 3 other platfroms made and optimized
     (need art)

---
+++ Assets/Scripts/PlayAnimatorOnEnter.cs
@@ -0,0 +1,32 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class PlayAnimatorOnEnter : MonoBehaviour
+{
+    public GameObject player;
+    public PlayableDirector timeline;
+
+    private void Start()
+    {
+        timeline = GetComponent<PlayableDirector>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            player.transform.parent = transform;
+            timeline.Play();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            player.transform.parent = null;
+        }
+    }
+}
