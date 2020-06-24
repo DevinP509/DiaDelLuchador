fc2dfaee24163a81118d352ed05a19a159cb2510
JacksonMossman <54289017+JacksonMossman@users.noreply.github.com>
2020-06-24 17:58:03 -0500

    Revert "Revert "Revert "Revert "Merge branch 'Devin' into Jax""""
    This reverts commit f0aff93e5a974cfb2d8bd9af5d3f677c26822d98.

---
+++ Assets/Scripts/PlatformChildingBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformChildingBehavior : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.transform.parent = transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.transform.parent = null;
+        }
+    }
+}
