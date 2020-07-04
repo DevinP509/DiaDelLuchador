398dabbbb6338c6e58b346e7d5bbcae43b7e9052
DevinP509 <54288889+DevinP509@users.noreply.github.com>
2020-07-04 11:48:38 -0500

    platforms spawn on destroy

--- Assets/Scripts/PlatformSpawnerBehavior.cs
+++ Assets/Scripts/PlatformSpawnerBehavior.cs
@@ -15,21 +15,19 @@ public class PlatformSpawnerBehavior : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        stopwatch.Start();
+        
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if(stopwatch.ElapsedMilliseconds /1000 >= SpawnTime)
-        {
-            int number = random.Next(0, PlatformPreFabs.Count);
-            UnityEngine.Debug.Log(number);
-            GameObject platform = PlatformPreFabs[number];
-            platform.GetComponent<PlatformFallingBehavior>().fallSpeed = platformFallRate;
-            Instantiate(platform, transform);
-            
-            stopwatch.Restart();
-        }
+    }
+
+    public void SpawnOnDestroy()
+    {
+        int number = random.Next(0, PlatformPreFabs.Count);
+        UnityEngine.Debug.Log(number);
+        GameObject platform = PlatformPreFabs[number];
+        platform.GetComponent<PlatformFallingBehavior>().fallSpeed = platformFallRate;
+        Instantiate(platform, transform);
     }
 }
