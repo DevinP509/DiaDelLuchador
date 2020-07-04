e9c4f4dd27a671075070080a8fa8c5e83e2045f2
JacksonMossman <54289017+JacksonMossman@users.noreply.github.com>
2020-07-04 11:48:07 -0500

    EnemyDetector

--- Assets/Scripts/PlatformSpawnerBehavior.cs
+++ Assets/Scripts/PlatformSpawnerBehavior.cs
@@ -11,16 +11,43 @@ public class PlatformSpawnerBehavior : MonoBehaviour
     public float platformFallRate;
     Stopwatch stopwatch = new Stopwatch();
     System.Random random = new System.Random();
+    public GameObject enemycounter;
+    private EnemeyDetector enemeyDetector;
+    public float FallRateNoEnemys;
+    
+    private int countHold;
 
     // Start is called before the first frame update
     void Start()
     {
+        enemeyDetector = enemycounter.GetComponent<EnemeyDetector>();
         stopwatch.Start();
     }
 
     // Update is called once per frame
     void Update()
     {
+      
+        if(enemeyDetector.count != countHold)
+        {
+            if (enemeyDetector.count <= 0)
+            {
+
+                PlatformFallingBehavior[] fallingplatforms = GetComponentsInChildren<PlatformFallingBehavior>();
+                for (int i = 0; i < fallingplatforms.Length; i++)
+                {
+                    fallingplatforms[i].fallSpeed = FallRateNoEnemys;
+                }
+            }
+            else
+            {
+                PlatformFallingBehavior[] fallingplatforms = GetComponentsInChildren<PlatformFallingBehavior>();
+                for (int i = 0; i < fallingplatforms.Length; i++)
+                {
+                    fallingplatforms[i].fallSpeed = platformFallRate;
+                }
+            }
+        }
         if (stopwatch.ElapsedMilliseconds / 1000 >= SpawnTime)
         {
             int number = random.Next(0, PlatformPreFabs.Count);
@@ -32,7 +59,8 @@ public class PlatformSpawnerBehavior : MonoBehaviour
             
 
             stopwatch.Restart();
-        }
+        } 
+        countHold = enemeyDetector.count;
     }
 }
   
