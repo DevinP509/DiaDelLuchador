6e3e733745b981ecb517dc5ada804484686b7edb
JacksonMossman <54289017+JacksonMossman@users.noreply.github.com>
2020-07-08 04:40:25 -0500

    Border Animator

--- Assets/BorderAnimator.cs
+++ Assets/BorderAnimator.cs
@@ -4,17 +4,33 @@ using UnityEngine;
 
 public class BorderAnimator : MonoBehaviour
 {
-    GameObject[] frames;
-    int frameCount;
+    public Texture[] textures;
+    int currentFrame = 0;
+    Renderer renderer;
+    float holderTime;
     // Start is called before the first frame update
     void Start()
     {
-        
+        renderer = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Time.time > holderTime + 1)
+        {
+            if(textures.Length > currentFrame)
+            {
+                currentFrame = 0;
+            }
+            else
+            {
+                currentFrame++;
+            }
+            
+            
+             renderer.material.mainTexture = textures[currentFrame];
+        }
+       
     }
 }
